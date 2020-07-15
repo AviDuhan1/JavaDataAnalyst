bea22a0a1168b83438e8c28af2204564fa94a8e1
AviDuhan1 <33582830+AviDuhan1@users.noreply.github.com>
2020-07-15 13:22:07 -0700

    Added a test file

--- src/DataTest.java
+++ src/DataTest.java
@@ -7,7 +7,7 @@ public class DataTest extends DataSetAnalyzer{
     public static void main(String[] args){
         //System.out.println(isNumeric("112345"));
         //System.out.println(isNumeric("Product1"));
-        File file = new File("SalesJan2009.csv");
+        File file = new File("TestSet.csv");
         double[][] dataset = newDataset(file);
         System.out.println(dataset.length + " " + dataset[0].length);
         for(int i = 0; i < dataset.length; i++){
